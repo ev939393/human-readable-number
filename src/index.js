fd90a0f4e295757bc9ac9b8cd8807d94c0b72439
Elena <102152365+ev939393@users.noreply.github.com>
2023-01-08 17:03:41 +0300

    feat: task <3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(n) {
+    let unit = ['000', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let dozen = ['000', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let elevenToNinteen = ['000', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen' , 'eighteen', 'nineteen'];
   
-}
+    str = String(n); 
+    
+    let a = str[0];
+    let b = str[1];
+    let c = str[2];
+  
+    let lastTwoDigits = Number(str.slice(-2));
+    
+    isMoreTenLessTwenty = lastTwoDigits > 10 && lastTwoDigits < 20;
+  
+    if(str.length === 3) {
+      if(n % 100 === 0) {return unit[a] + ' hundred'}
+      if(n % 10 === 0) {return unit[a] + ' hundred ' + dozen[b]}
+      if(isMoreTenLessTwenty) {return unit[a] + ' hundred ' + elevenToNinteen[c]}
+      if(b == 0) {return unit[a] + ' hundred ' + unit[c]}
+      return unit[a] + ' hundred ' + dozen[b] + ' ' + unit[c]
+    }
+  
+    if(str.length === 2) {
+      if(n % 10 === 0) {return dozen[a]}
+      if(isMoreTenLessTwenty) {return elevenToNinteen[b]}
+      return dozen[a] + ' ' + unit[b]
+    }
+  
+    if(str.length === 1) {
+      return unit[a];
+    }  
+  }
